ef380621cf85728b7131cb592d7ea544818eb993
mariekeller <mariekeller357@gmail.com>
2016-03-13 21:44:41 +0600

    добавлено вычисление корней квадратного уравнения

--- main.c
+++ main.c
@@ -5,4 +5,27 @@
 void main ()
 {
 	setlocale(LC_ALL, "Russian");
+	printf("Введите коэффициенты a, b, c:\n");
+	float a, b, c;
+	scanf("%f %f %f", &a, &b, &c);
+	float d;
+	d = b * b - 4 * a * c;
+	float x1, x2;
+	if (!a)
+		printf("\nКоэффициент a не должен быть равен 0\n");
+	else
+		if (d > 0)
+		{
+			x1 = (-b + sqrt(d)) / (2 * a);
+			x2 = (-b - sqrt(d)) / (2 * a);
+			printf("\nx1 = %.3f, x2 = %.3f\n",x1, x2);
+		}
+		else
+			if (d == 0)
+			{
+				x1 = x2 = -b / (2 * a);
+				printf("x1 = x2 = %.3f\n", x1);
+			}
+			else
+				printf("\nОтрицательный дискриминант\n");
 }
